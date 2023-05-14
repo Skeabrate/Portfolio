62dd49ebc2045b6ff916910810d81c2935e9c61c
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-05-15 00:17:51 +0200

    Deleted debounce fn

--- src/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-export function debounce(func: () => void, delay = 300) {
-  let timer: ReturnType<typeof setTimeout>;
-
-  return function () {
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(func, delay);
-  };
-}
